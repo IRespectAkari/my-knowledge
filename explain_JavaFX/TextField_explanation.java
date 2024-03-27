1dc886fc9632f77c9a444d820877bcdf2d7f598e
IRespectAkari <145774048+IRespectAkari@users.noreply.github.com>
2024-03-27 17:40:48 +0900

    Create TextField_explanation.java

---
+++ explain_JavaFX/TextField_explanation.java
@@ -0,0 +1,12 @@
+TextField クラスの説明
+TextField()
+TextField(String init)
+
+ + getText() : String// テキストの取得
+ + setText(String) : void// テキストの変更
+ + setPromptText(String) : void// プロンプトテキスト（薄いグレー等で表示されるガイドとなるテキスト）の設定
+
+Button クラスの説明
+
+Button()
+Button(String)// 引数を表示テキストとする
